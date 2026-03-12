1d5d61b9f28e802a63422cd2a56454bcb0712bb6
joaovmourasouza <joaovmoura.dsouza@gmail.com>
2026-03-12 06:12:57 -0300

    feat: Add KinesisStack to manage Firehose delivery to S3 and refactor its associated IAM role.

--- bootcamp-AndreSioneck/bootcamp-data-platform/app.py
+++ bootcamp-AndreSioneck/bootcamp-data-platform/app.py
@@ -4,6 +4,7 @@ import aws_cdk as cdk
 from bootcamp_data_platform.bootcamp_data_platform_stack import BootcampDataPlatformStack
 from data_platform.common_stack import CommonStack
 from data_platform.enviroment import EnviromentEnum
+from data_platform.kinesis import KinesisStack
 
 app = cdk.App()
 
@@ -11,5 +12,6 @@ deploy_env = EnviromentEnum(app.node.try_get_context("deploy_enviroment"))
 
 common_stack = CommonStack(app, "CommonStack", enviroment=deploy_env)
 data_platform_stack = BootcampDataPlatformStack(app, "BootcampDataPlatformStack", enviroment=deploy_env)
+kinesis = KinesisStack(app,  data_lake_raw=data_platform_stack.data_lake_raw, enviroment=deploy_env)
 
 app.synth()
--- bootcamp-AndreSioneck/bootcamp-data-platform/data_platform/kineses/stack.py
+++ bootcamp-AndreSioneck/bootcamp-data-platform/data_platform/kineses/stack.py
@@ -1,26 +1,82 @@
-from aws_cdk import core as cdk
+from constructs import Construct
 from aws_cdk import aws_iam as iam
 from aws_cdk import aws_kinesis as kinesis
-from bootcamp_data_platform import active_enviroment, environment
-from bootcamp_data_platform.base import DataLakeBase
 
-class  RawKinesisRole(iam.Role):
-    def __init__(self, scope: cdk.Construct, deploy_env: enviroment, data_lake_raw: DataLakeBase, **kwargs) -> None:
+from data_platform.enviroment import EnviromentEnum
+from data_platform.data_lake.base import DataLakeBase
+
+class RawKinesisRole(iam.Role):
+    def __init__(self, scope: Construct, deploy_env: EnviromentEnum, data_lake_raw: DataLakeBase, **kwargs) -> None:
         self.deploy_env = deploy_env
         self.data_lake_raw = data_lake_raw
-        super().__init__(scope, 
-        id=f'{self.deploy_env.value}-raw-kinesis-role',
-        assumed_by=iam.ServicePrincipal('kinesis.amazonaws.com'),
-        description=f'Role for raw kinesis stream in {self.deploy_env.value} environment',
-        **kwargs)
-        self.add_to_policy(iam.PolicyStatement(
-            actions=[
-                's3:PutObject',
-                's3:GetObject',
-                's3:ListBucket'
-            ],
-            resources=[
-                self.data_lake_raw.bucket_arn,
-                f'{self.data_lake_raw.bucket_arn}/*'
+        super().__init__(
+            scope, 
+            id=f'{self.deploy_env.value}-raw-kinesis-role',
+            assumed_by=iam.ServicePrincipal('kinesis.amazonaws.com'),
+            description=f'Role for raw kinesis stream in {self.deploy_env.value} environment',
+            **kwargs
+        )
+        self.set_policy()
+
+    def set_policy(self) -> None:
+        policy = iam.Policy(
+            self,
+            id=f'{self.deploy_env.value}-raw-kinesis-policy',
+            policy_name=f'{self.deploy_env.value}-raw-kinesis-policy',
+            statements=[
+                iam.PolicyStatement(
+                    actions=[
+                        's3:PutObject',
+                        's3:GetObject',
+                        's3:ListBucket',
+                        's3:AbortMultipartUpload',
+                        's3:DeleteObject'
+                    ],
+                    resources=[
+                        self.data_lake_raw.bucket_arn,
+                        f'{self.data_lake_raw.bucket_arn}/*'
+                    ]
+                )
             ]
-        ))
+        )
+        self.attach_inline_policy(policy)
+
+class KinesisStack(cdk.Stack):
+    def __init__(self, scope: Construct, deploy_env: EnviromentEnum, data_lake_raw: DataLakeBase, **kwargs) -> None:
+        self.deploy_env = deploy_env
+        self.data_lake_raw = data_lake_raw
+        super().__init__(
+            scope,
+            id=f'{self.deploy_env.value}-kinesis-stack',
+            description=f'Kinesis stack for {self.deploy_env.value} environment',
+            **kwargs)
+
+        self.atomic_events = firehose.CfnDeliveryStream(
+            self,
+            id = f'firehose-{self.deploy_env.value}-raw-delivery-stream',
+            delivery_stream_type='DirectPut',
+            delivery_stream_name=f'firehose-{self.deploy_env.value}-raw-delivery-stream',
+            extended_s3_destination_configuration=self.s3_config
+        )
+
+    @property
+    def s3_config(self):
+        return firehose.CfnDeliveryStream.ExtendedS3DestinationConfigurationProperty(
+            bucket_arn=self.data_lake_raw.bucket_arn,
+            buffering_hints=firehose.CfnDeliveryStream.BufferingHintsProperty(
+                interval_in_seconds=60,
+                size_in_mbs=1
+            ),
+            compression_format='GZIP',
+            error_output_prefix=f'{self.deploy_env.value}/errors',
+            prefix=f'{self.deploy_env.value}/raw/',
+            role_arn=self.raw_kinesis_role.role_arn
+        )
+
+    @property
+    def kinesis_role(self):
+        return RawKinesisRole(
+            self,
+            deploy_env=self.deploy_env,
+            data_lake_raw=self.data_lake_raw
+        )
